4c7e8c943a34789584eb0c5c3913e9552a2ff923
Ingo Renner <ingo@typo3.org>
2014-02-12 12:00:28 -0800

    Add a recipe with tasks for Vagrant boxes

---
+++ recipes/vagrant.rb
@@ -0,0 +1,29 @@
+#
+# Author:: Ingo Renner (<ingo@typo3.org>)
+# Cookbook Name:: typo3
+# Recipe:: vagrant
+#
+# Copyright 2014, Ingo Renner
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# Add the vagrant user to the Apache group
+group node['apache']['group'] do
+  action :modify
+  membders "vagrant"
+  append true
+end
+
+
