92368417a386a4a731fb4d6519c118b61b91cdba
Ingo Renner <ingo@typo3.org>
2014-02-11 18:05:33 -0800

    Increase version to 0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ingo@typo3.org'
 license          'Apache 2.0'
 description      'Installs/Configures TYPO3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 
 
 %w{
